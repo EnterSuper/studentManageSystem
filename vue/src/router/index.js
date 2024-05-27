9c923035c1dddd00215d1ce1f1227d2e264ea402
EnterSuper <2127738288@qq.com>
2024-05-27 10:54:37 +0800

    多角色权限访问

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -45,5 +45,10 @@ router.beforeEach((to, from, next) => {
     next()
   }
 })
+// 解决导航栏或者底部导航tabBar中的vue-router在3.0版本以上频繁点击菜单报错的问题。
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push (location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
 export default router
